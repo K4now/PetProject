af73b7c8854321d80e7cf585710a0be376c8567c
K4now <77333584+K4now@users.noreply.github.com>
2025-06-05 13:19:22 +0300

    fix

--- lib/welcome/widgets/card_widget.dart
+++ lib/welcome/widgets/card_widget.dart
@@ -21,57 +21,58 @@ class CardWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.2,
-          child: InkWell(
-            onTap: () {
-              onPressed();
-            },
-            child: Card(
-              color: color,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.30,
-                        height: MediaQuery.of(context).size.height * 0.30,
-                        child: child,
-                      ),
+        height: MediaQuery.of(context).size.height * 0.2,
+        child: InkWell(
+          onTap: () {
+            onPressed();
+          },
+          child: Card(
+            color: color,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.30,
+                      height: MediaQuery.of(context).size.height * 0.30,
+                      child: child,
                     ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.02,
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            flex: 0,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.021,
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 0,
+                          child: Text(
+                            name,
+                            style: TextStyle(fontSize: 18, color: colorText),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: SingleChildScrollView(
                             child: Text(
-                              name,
-                              style: TextStyle(fontSize: 18, color: colorText),
+                              description,
+                              style: TextStyle(fontSize: 15, color: colorText),
                             ),
                           ),
-                          Expanded(
-                            flex: 2,
-                            child: SingleChildScrollView(
-                              child: Text(
-                                description,
-                                style: TextStyle(fontSize: 15, color: colorText),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-          ),),
+          ),
+        ),
+      ),
     );
   }
 }
