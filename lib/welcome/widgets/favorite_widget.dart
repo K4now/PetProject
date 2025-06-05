352d48e38fb083bc3a451485073d497e48fd258e
K4now <77333584+K4now@users.noreply.github.com>
2025-06-05 18:33:33 +0300

    fix

--- lib/welcome/widgets/favorite_widget.dart
+++ lib/welcome/widgets/favorite_widget.dart
@@ -1,8 +1,9 @@
+import 'package:telegram_web_app/telegram_web_app.dart';
+
 import '../data/card_class.dart';
 import 'package:test_project/welcome/widgets/card_widget.dart';
 
 import 'package:flutter/material.dart';
-
 
 class FavoriteWidget extends StatelessWidget {
   const FavoriteWidget({
@@ -10,26 +11,35 @@ class FavoriteWidget extends StatelessWidget {
     required this.listCards,
   });
 
-
-final List<CardClass> listCards;
-  
+  final List<CardClass> listCards;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: listCards.length,
-      itemBuilder: (context, index) {
-        return CardWidget(
-          name: listCards[index].name,
-          description: listCards[index].description,
-          onPressed: () {
-            listCards[index].onPressed();
+    return Column(
+      children: [
+        // current telegram version
+        Text(TelegramWebApp.instance.version),
+
+        Text(TelegramWebApp.instance.themeParams.toString()),
+
+// Object containing user details and user validation hash
+        Text(TelegramWebApp.instance.initData.toString()),
+        ListView.builder(
+          itemCount: listCards.length,
+          itemBuilder: (context, index) {
+            return CardWidget(
+              name: listCards[index].name,
+              description: listCards[index].description,
+              onPressed: () {
+                listCards[index].onPressed();
+              },
+              color: listCards[index].color,
+              colorText: listCards[index].colorText,
+              child: listCards[index].child,
+            );
           },
-          color: listCards[index].color,
-          colorText: listCards[index].colorText,
-          child: listCards[index].child,
-        );
-      },
+        ),
+      ],
     );
   }
 }
